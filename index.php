a2deaa29afce8855e7437cf9f0fffc87d2edc2e4
jorge <jorgedipra@gmail.com>
2013-07-09 21:02:19 -0500

    esqueleto para diseño

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
-<?php 
+<?php
+ include_once ('recursos/info.php');//se llama la informacion de la pagina
 session_start();
+?>
+<link rel="stylesheet" href="css/style.css"><!--se llama el stilo-->
+<script src="script/scriptpins.js"></script>
+<title> </title>
+</head>
+<body>
+<?php
 include_once('conexion/php/operacionesSql.php');
   $objopera  =  new operaciones();
   if(isset($_POST['nick'])){
@@ -13,13 +21,13 @@ include_once('conexion/php/operacionesSql.php');
             else{
             	header('location: administrador/administradorprincipal.php');
             }
-        }
-     
+        }     
   }
    if(isset($_POST['asunto'])){
 	  $consul="INSERT INTO Pregunta(PreAsunto,PreDescipcion,PreCorreo)VALUES('".$_POST['asunto']."','".$_POST['pregunta']."','".$_POST['correo']."')";
 	  $objopera->insertar($consul);
 	}
-include_once('cuerpo/index.php');
-
+  include_once('cuerpo/index.php');
 ?>
+</body>
+</html>
---
+++ recursos/info.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="utf-8" /> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Page-Enter" content="blendtrans(duration=1)">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+<meta name="viewport" content="width = device-width, initial-scale=1, maximum-scale=1"/>
+<meta name="description" content="">
+<meta property="og:title" content="" />
+<meta property="og:type" content="" />
+<meta property="og:url" content=""/>
+<meta property="og:image" content="" />
+<meta property="og:site_name" content="" />
+<link rel="shortcut icon" href="/img/h1.ico" />

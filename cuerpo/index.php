739fcc153b6d0b6fc0e1614c69d7d8a1fb431121
jorge <jorgedipra@gmail.com>
2013-07-09 21:12:28 -0500

    tab-fondo
    se coloco el fondo y se organizo las tab

--- cuerpo/index.php
+++ cuerpo/index.php
@@ -1,14 +1,37 @@
 <a href="#">REGISTRARSE</a>
 <a href="#">INGRESAR</a>
 
-<div id="menu">
-	<ul>
-		<li>INICIO</li>
-		<li>VISION</li>
-		<li>MISION</li>
-		<li>SERVICIO</li>
-	</ul>
+
+
+<div id="tabs">
+  <ul>
+    <li><a href="#tabs-1">INICIO</a></li>
+    <li><a href="#tabs-2">VISION</a></li>
+    <li><a href="#tabs-3">MISION</a></li>
+    <li><a href="#tabs-4">SERVICIO</a></li>
+  </ul>
+  <div id="tabs-1">
+    <p>
+        1
+    </p>
+  </div>
+  <div id="tabs-2">
+    <p>
+        2
+    </p>
+  </div>
+  <div id="tabs-3">
+    <p>
+        3
+    </p>
+  </div>
+  <div id="tabs-4">
+    <p>
+        4
+    </p>
+  </div>
 </div>
+
 <section id="logeo">
 	<form action="index.php" method="post">
 		<label>Nick</label>
--- index.php
+++ index.php
@@ -3,7 +3,15 @@
 session_start();
 ?>
 <link rel="stylesheet" href="css/style.css"><!--se llama el stilo-->
-<script src="script/scriptpins.js"></script>
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+  <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+  <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+  <link rel="stylesheet" href="/resources/demos/style.css" />
+  <script>
+  $(function() {
+    $( "#tabs" ).tabs();
+  });
+  </script>
 <title> </title>
 </head>
 <body>
